4246e2bcfd6bbfa601deebe9dca95b521bc8256f
Breno Nunes <brenontavares@gmail.com>
2021-06-25 19:49:16 -0300

    add conditional in actions

--- plop-scripts/page.js
+++ plop-scripts/page.js
@@ -6,40 +6,53 @@ module.exports = {
       name: "pageName",
       message: "Qual será o nome da sua pagina?",
     },
-  ],
-  actions: [
-    // criando arquivo index.js da pagina
     {
-      type: "add",
-      path: "src/pages/{{pascalCase name}}/index.js",
-      templateFile: "plop-templates/page/index.hbs",
-    },
-    // injetando exportação em pages/index.js
-    {
-      type: "append",
-      path: "src/pages/index.js",
-      pattern: `/* PLOP_PAGE_EXPORT */`,
-      template: `export { default as {{pascalCase name}} } from "./{{pascalCase name}}"`,
-    },
-    // criando arquivo styles.js da pagina
-    {
-      type: "add",
-      path: "src/pages/{{pascalCase name}}/styles.js",
-      templateFile: "plop-templates/page/styles.hbs",
-    },
-    // injetando importação da pagina em routes.js
-    {
-      type: "append",
-      path: "src/routes.js",
-      pattern: `/* PLOP_ROUTE_IMPORT */`,
-      template: `\t{{pascalCase name}},`,
-    },
-    // injetando route da pagina em routes.js
-    {
-      type: "append",
-      path: "src/routes.js",
-      pattern: `{/* PLOP_INJECT_ROUTE */}`,
-      template: `\t\t\t\t<Route exact path="/{{dashCase name}}" component={ {{pascalCase name}} } />`,
+      type: "confirm",
+      name: "hasStyle",
+      message: "Você quer gerar o arquivo de estilo?",
     },
   ],
+  actions: function (data) {
+    let actions = [
+      // criando arquivo index.js da pagina
+      {
+        type: "add",
+        path: "src/pages/{{pascalCase pageName}}/index.js",
+        templateFile: "plop-templates/page/index.hbs",
+      },
+      // injetando exportação em pages/index.js
+      {
+        type: "append",
+        path: "src/pages/index.js",
+        pattern: `/* PLOP_PAGE_EXPORT */`,
+        template: `export { default as {{pascalCase pageName}} } from "./{{pascalCase pageName}}"`,
+      },
+
+      // injetando importação da pagina em routes.js
+      {
+        type: "append",
+        path: "src/routes.js",
+        pattern: `/* PLOP_ROUTE_IMPORT */`,
+        template: `\t{{pascalCase pageName}},`,
+      },
+      // injetando route da pagina em routes.js
+      {
+        type: "append",
+        path: "src/routes.js",
+        pattern: `{/* PLOP_INJECT_ROUTE */}`,
+        template: `\t\t\t\t<Route exact path="/{{dashCase pageName}}" component={ {{pascalCase pageName}} } />`,
+      },
+    ];
+
+    if (data.hasStyle) {
+      // criando arquivo styles.js da pagina
+      actions.push({
+        type: "add",
+        path: "src/pages/{{pascalCase pageName}}/styles.js",
+        templateFile: "plop-templates/page/styles.hbs",
+      });
+    }
+
+    return actions;
+  },
 };
